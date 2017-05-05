11d3c2ab5d8e5e50d395c6f4e007bcdbc79b0e0c
uicn <uicn@MacBook-Pro.local>
2017-05-05 18:32:38 +0800

    notes

--- app/Http/Controllers/Portal/BlogController.php
+++ app/Http/Controllers/Portal/BlogController.php
@@ -9,10 +9,18 @@ use App\Http\Models\Blog;
 
 class BlogController extends Controller
 {
+    /**
+     * blog list
+     * @return Response
+     */
     public function index (){
       return view('portal.blogs.index')->with('blogs'=>Blog::paginate(15));
     }
-
+    /**
+     * information of blog by id
+     * @param $id int
+     * @return arr response
+     */
     public function information(Request $request, $id){
       return view('portal.blogs.information',['blogs'=>Blog::find($id),
                                               'tag'=>Tag::where('bid'=>$id)->get(),
